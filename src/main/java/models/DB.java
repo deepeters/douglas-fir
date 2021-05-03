5a0a1984426f257291f1c9c6fc6d6d181b75f942
Dennis Peters <dennis95peters@gmail.com>
2021-05-03 10:31:23 +0300

    This commit attemps to debug minor errors in the code

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -4,7 +4,7 @@ import org.sql2o.*;
 
 public class DB {
     // For local use only
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "deepeters", "");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "deepeters", "password");
 
     //private static String connectionString = "jdbc:postgresql://ec2-52-71-231-180.compute-1.amazonaws.com:5432/d7ibdughsrddip";
     //public static Sql2o sql2o = new Sql2o(connectionString, "ksqytqouusqfct", "4cbd75780f8534cc6bd66e462eadcfc7337589ea905d9bc39c7a54826b37189b");

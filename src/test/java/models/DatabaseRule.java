a1d99a624937621e7d2b990a620f3826e2d21438
Dennis Peters <dennis95peters@gmail.com>
2021-05-03 06:17:04 +0300

    This commit creates a database rule

---
+++ src/test/java/models/DatabaseRule.java
@@ -0,0 +1,11 @@
+package models;
+
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends  ExternalResource{
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "deepeters", "password");
+    }
+}

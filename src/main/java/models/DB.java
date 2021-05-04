627ab14681cc7768e33e197b13ee4aba82ac97f1
Dennis Peters <dennis95peters@gmail.com>
2021-05-04 05:09:56 +0300

    This commit debugs the code to ensure application runs seamlessly

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -4,8 +4,11 @@ import org.sql2o.*;
 
 public class DB {
     // For local use only
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "deepeters", "password");
+    // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "deepeters", "password");
 
-    //private static String connectionString = "jdbc:postgresql://ec2-52-71-231-180.compute-1.amazonaws.com:5432/d7ibdughsrddip";
-    //public static Sql2o sql2o = new Sql2o(connectionString, "ksqytqouusqfct", "4cbd75780f8534cc6bd66e462eadcfc7337589ea905d9bc39c7a54826b37189b");
+    //For Heroku
+    private static String connectionString = "jdbc:postgresql://ec2-54-196-33-23.compute-1.amazonaws.com:5432/da4vsocdcsgf2g"; //!
+    public static Sql2o sql2o = new Sql2o(connectionString, "jjnbbjicfjdupw", "0ac59a91dd41d91b98dad8a9051c210d042ff23685ae938a87c8fd69839cd0ca"); //!
 }
+
+

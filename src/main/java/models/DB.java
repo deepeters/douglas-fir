313c6fc39fed9cccad35f0ac3c304569aa9ceab6
Dennis Peters <dennis95peters@gmail.com>
2021-05-04 05:21:24 +0300

    This commit attempts to link heroku database

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -7,8 +7,8 @@ public class DB {
     // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "deepeters", "password");
 
     //For Heroku
-    private static String connectionString = "jdbc:postgresql://ec2-54-196-33-23.compute-1.amazonaws.com:5432/da4vsocdcsgf2g"; //!
-    public static Sql2o sql2o = new Sql2o(connectionString, "jjnbbjicfjdupw", "0ac59a91dd41d91b98dad8a9051c210d042ff23685ae938a87c8fd69839cd0ca"); //!
+    private static String connectionString = "jdbc:postgresql://ec2-52-87-107-83.compute-1.amazonaws.com:5432/dc04vf9j5qb3g5"; //!
+    public static Sql2o sql2o = new Sql2o(connectionString, "fkpzwbhosrazfb", "82b3a75d0143686c51bbfe50f2a6f293e40f7335887565bfb05fd1b42c2d5a0f"); //!
 }
 
 
